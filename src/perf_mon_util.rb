c487a698d8fe8c4b7fffa49a7a80639764655519
Cheng Chi (@joychester) <joychester@gmail.com>
2011-07-14 11:35:04 +0800

    Add TCP connections info

--- src/perf_mon_util.rb
+++ src/perf_mon_util.rb
@@ -1,7 +1,3 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
-
 class PerfMonUtil
   @hostIPs = [];
 
@@ -35,13 +31,12 @@ class PerfMonUtil
               "\\physicaldisk(_total)\\% disk read time", #DISK I/O
               "\\physicaldisk(_total)\\% disk write time",
               "\\network interface(*#1)\\bytes total/sec", #NetWork I/O
+              "\\TCP\\Connections Established", #TCP Connections counts for ESTABLISHED or CLOSE-WAIT state
               "-s", "#{machineIP}","-si","#{intv}", "-sc", "#{sampler_count}", "-o", filename)
 
               Process.exit!(1);
        }
     }
-    
+
   end
 end
-
-
